8c7e63482bf4bafd8afca4cc781f6077e1323b7e
professor_cypher <professorcodegeek@DESKTOP-87DPNVK.localdomain>
2023-02-01 22:50:57 +0200

    escrow in cluster1

---
+++ cluster1/code-challenges/escrow_program/src/error.rs
@@ -0,0 +1,33 @@
+use thiserror::Error ;
+use solana_program::program_error::ProgramError;
+
+#[derive(Error,Debug, Copy, Clone)]
+pub enum EscrowError {
+  ///Invalid instruction
+ #[error("Invalid Instruction")]
+ InvalidInstruction,
+
+ //Not rent Exempt 
+ #[error("Not rent Exempt")]
+ NotRentExempt,
+
+ //Expected Amount Mismatch
+ #[error("Expected Amount Mismatch")]
+ ExpectedAmountMismatch,
+
+ //Escrow Not Unlocked Yet
+ #[error("Escrow Not Unlocked Yet")]
+ EscrowNotUnlocked,
+
+ //Escrow Timed Out
+ #[error("Escrow Timed Out")]
+ EscrowTimedOut,
+
+}
+
+impl From<EscrowError> for ProgramError {
+  fn from(e: EscrowError) -> Self {
+    ProgramError::Custom(e as u32)
+  }
+
+}
