0c8e216e4786edc98b3813033a1e54cf837f2d65
professor_cypher <professorcodegeek@DESKTOP-87DPNVK.localdomain>
2023-03-09 10:51:23 +0200

    initialize instruction struct begining

--- capstone/plant-tree/programs/starter/src/lib.rs
+++ capstone/plant-tree/programs/starter/src/lib.rs
@@ -6,14 +6,38 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod starter {
     use super::*;
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-
         // ctx.accounts.user.
         Ok(())
     }
 }
 
+
+#[account]
+pub struct Tree {
+    name: String,
+    location: Land 
+}
+
+#[account]
+pub struct Land {
+    owner: Pubkey,
+    is_planted: bool,
+}
+
+#[account]
+pub struct Farmer {
+    name: String,
+    land_areas: Vec<(u64,u64)>,
+    trees: Vec<Tree>,
+    address: Pubkey
+}
+
 #[derive(Accounts)]
 pub struct Initialize <'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    #[account(init, payer=payer, seeds=["farmer", payer.key().to_bytes()], bump, space = )]
+    pub farmer: Account<'info,Farmer>,
     #[account(mut)]
     pub user: SystemAccount<'info>,
 }
